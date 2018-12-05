41d40d82b76008cbf5dbb75a476e4c906e22cb73
Ben Cowing <spamman138x@gmail.com>
2018-12-05 16:17:46 -0600

    fuk it

--- app/src/main/java/com/example/cmala/pokerapp/Game.java
+++ app/src/main/java/com/example/cmala/pokerapp/Game.java
@@ -9,9 +9,15 @@ public class Game  extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.game_screen);
 
         Intent intent = getIntent();
-         numberOfCpus = intent.getIntExtra("numberCpus", 1);
+        numberOfCpus = intent.getIntExtra("numberCpus", 1);
+        if (numberOfCpus == 1) {
+            setContentView(R.layout.one_cpugame_screen);
+        } else if (numberOfCpus == 2) {
+            setContentView(R.layout.two_cpugame_screen);
+        } else {
+            setContentView(R.layout.three_cpugame_screen);
+        }
     }
 }

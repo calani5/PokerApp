046fa638827d20afb9d18b9464e7c34cd51645a1
calani5 <calani5@illinois.edu>
2018-12-09 16:57:53 -0600

    cleaning

--- app/src/main/java/com/example/cmala/pokerapp/Deck.java
+++ app/src/main/java/com/example/cmala/pokerapp/Deck.java
@@ -1,16 +1,5 @@
 package com.example.cmala.pokerapp;
 
-import android.app.Activity;
-import android.content.Context;
-import android.os.Bundle;
-import android.util.Log;
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.Volley;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
